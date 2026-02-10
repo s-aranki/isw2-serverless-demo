3783da1135101d8c71221ef0bfbe2010006bda03
Samer Aranki <79375749+s-aranki@users.noreply.github.com>
2026-02-09 21:35:40 -0600

    Update procesar.js

--- api/procesar.js
+++ api/procesar.js
@@ -2,7 +2,7 @@ export default function handler(req, res) {
   const nombre = req.query.nombre || "anónimo";
 
   res.status(200).json({
-    resultado: `Nombre procesado: ${nombre.toUpperCase()}`
-    // resultado: `Nombre procesado: ${nombre.toLowerCase()}`
+    resultado: `Nombre procesado: ${nombre.toUpperCase()}`,
+    longitud: nombre.length
   });
 }
